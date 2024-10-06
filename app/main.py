474fc8999422707e22b2f7718d68e8e8bf059236
jaxayM <missour1998@gmail.com>
2024-10-06 20:05:02 +0200

    lexical errors

--- app/main.py
+++ app/main.py
@@ -14,28 +14,35 @@ def main():
         exit(1)
 
     with open(filename) as file:
-        file_contents = file.read()
-
-    if file_contents:
-        for c in file_contents:
-            if c in ['(',')']:
-                print(f"{'LEFT' if c=='(' else 'RIGHT'}_PAREN {c} null")
-            if c in ['{','}']:
-                print(f"{'LEFT' if c=='{' else 'RIGHT'}_BRACE {c} null")
-            if c=='*':
-                print("STAR * null")
-            if c=='.':
-                print("DOT . null")
-            if c==',':
-                print("COMMA , null")
-            if c=='+':
-                print("PLUS + null")
-            if c=='-':
-                print("MINUS - null")
-            if c==';':
-                print("SEMICOLON ; null")
+        error = False
+        for i, line in enumerate(file):
+            for c in line:
+                if c in ['(',')']:
+                    print(f"{'LEFT' if c=='(' else 'RIGHT'}_PAREN {c} null")
+                if c in ['{','}']:
+                    print(f"{'LEFT' if c=='{' else 'RIGHT'}_BRACE {c} null")
+                if c=='*':
+                    print("STAR * null")
+                if c=='.':
+                    print("DOT . null")
+                if c==',':
+                    print("COMMA , null")
+                if c=='+':
+                    print("PLUS + null")
+                if c=='-':
+                    print("MINUS - null")
+                if c==';':
+                    print("SEMICOLON ; null")
+                
+                if c in ['$','#']:
+                    print(f"[line {i}] Error: Unexpected character: {c}", file=sys.stderr)
+                    error = True
 
     print("EOF  null")
+    if error:
+        exit(65)
+    else:
+        exit(0)
 
 
 if __name__ == "__main__":
