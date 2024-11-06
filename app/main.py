dfac4e0bc211aa946f4af05235fa0aef5b168ba2
jaxayM <missour1998@gmail.com>
2024-11-06 18:39:59 +0100

    parse parentheses

--- app/main.py
+++ app/main.py
@@ -1,4 +1,23 @@
 import sys
+from enum import Enum
+
+class TokenType(Enum):
+    PAREN = 1
+    BRACE = 2
+    STAR = 3
+    STRING = 4
+    NUMBER = 5
+    IDENTIFIER = 6
+    UNARY = 7
+    EQUAL = 8
+    SEMI = 9
+
+    EOF = 99
+
+class Token:
+    def __init__(self, type, value):
+        self.token_type = type
+        self.value = value
 
 
 def main():
@@ -10,18 +29,11 @@ def main():
     filename = sys.argv[2]
 
     commands = ["parse", "tokenize"]
-    if command == "parse":
-        with open(filename) as f:
-            content = f.read()
-            if content in ["nil", "true", "false"]:
-                print(content)
-            else:
-                print(float(content))
-            exit(0)
     if command not in commands:
         print(f"Unknown command: {command}", file=sys.stderr)
         exit(1)
 
+    tokens = []
     keywords = ["and", "class", "else", "false", "for", "fun", "if", "nil", "or", "print", "return", "super", "this", "true", "var", "while"]
     with open(filename) as file:
         error = False
@@ -32,29 +44,33 @@ def main():
                     jump = jump - 1
                     continue
                 if c in ['(',')']:
-                    print(f"{'LEFT' if c=='(' else 'RIGHT'}_PAREN {c} null")
+                    tokens.append(Token(TokenType.PAREN, c))
+                    if command == "tokenize": print(f"{'LEFT' if c=='(' else 'RIGHT'}_PAREN {c} null")
                 elif c in ['{','}']:
-                    print(f"{'LEFT' if c=='{' else 'RIGHT'}_BRACE {c} null")
+                    tokens.append(Token(TokenType.BRACE, c))
+                    if command == "tokenize": print(f"{'LEFT' if c=='{' else 'RIGHT'}_BRACE {c} null")
                 elif c=='*':
-                    print("STAR * null")
+                    tokens.append(Token(TokenType.STAR, "null"))
+                    if command == "tokenize": print("STAR * null")
                 elif c=='.':
-                    print("DOT . null")
+                    if command == "tokenize": print("DOT . null")
                 elif c==',':
-                    print("COMMA , null")
+                    if command == "tokenize": print("COMMA , null")
                 elif c=='+':
-                    print("PLUS + null")
+                    if command == "tokenize": print("PLUS + null")
                 elif c=='-':
-                    print("MINUS - null")
+                    if command == "tokenize": print("MINUS - null")
                 elif c==';':
-                    print("SEMICOLON ; null")
+                    tokens.append(Token(TokenType.SEMI, "null"))
+                    if command == "tokenize": print("SEMICOLON ; null")
                 elif c==' ' or c=='\t' or c=='\n':
                     continue
                 elif c=='=':
                     if line[w:w+2] == '==':
-                        print("EQUAL_EQUAL == null")
+                        if command == "tokenize": print("EQUAL_EQUAL == null")
                         jump = 1
                     else:
-                        print("EQUAL = null")
+                        if command == "tokenize": print("EQUAL = null")
                 elif c=='"':
                     string = ""
                     unterminated = True
@@ -66,41 +82,43 @@ def main():
                         else:
                             string = string + line[j]
                     if unterminated:
-                        print("[line %s] Error: Unterminated string." % (i+1), file=sys.stderr)
+                        if command == "tokenize": print("[line %s] Error: Unterminated string." % (i+1), file=sys.stderr)
                         error = True
                         break
                     else:
-                        print(f'STRING "{string}" {string}')
+                        tokens.append(Token(TokenType.STRING, string))
+                        if command == "tokenize": print(f'STRING "{string}" {string}')
                 elif c=='!':
                     if line[w:w+2] == '!=':
-                        print("BANG_EQUAL != null")
+                        if command == "tokenize": print("BANG_EQUAL != null")
                         jump = 1
                     else:
-                        print("BANG ! null")
+                        if command == "tokenize": print("BANG ! null")
                 elif c=='<':
                     if line[w:w+2] == '<=':
-                        print("LESS_EQUAL <= null")
+                        if command == "tokenize": print("LESS_EQUAL <= null")
                         jump = 1
                     else:
-                        print("LESS < null")
+                        if command == "tokenize": print("LESS < null")
                 elif c=='>':
                     if line[w:w+2] == '>=':
-                        print("GREATER_EQUAL >= null")
+                        if command == "tokenize": print("GREATER_EQUAL >= null")
                         jump = 1
                     else:
-                        print("GREATER > null")
+                        if command == "tokenize": print("GREATER > null")
                 elif c=='/':
                     if line[w:w+2] == '//':
                         break
                     else:
-                        print("SLASH / null")
+                        if command == "tokenize": print("SLASH / null")
                 elif c in map(lambda s: str(s) ,list(range(10))):
                     for j in range(w+1, len(line)):
                         if line[j] in map(lambda s: str(s) ,list(range(10))) or line[j] == '.':
                             jump += 1
                         else:
                             break
-                    print(f'NUMBER {line[w:w+jump+1]} {float(line[w:w+jump+1])}')
+                    tokens.append(Token(TokenType.NUMBER, line[w:w+jump+1]))
+                    if command == "tokenize": print(f'NUMBER {line[w:w+jump+1]} {float(line[w:w+jump+1])}')
                 elif c.isalpha() or c=='_':
                     for j in range(w+1, len(line)):
                         if line[j].isalnum() or line[j]=='_':
@@ -109,14 +127,24 @@ def main():
                             break
                     kw = line[w:w+jump+1]
                     if kw in keywords:
-                        print(f"{kw.upper()} {kw} null")
-                    else: print(f"IDENTIFIER {kw} null")
+                        tokens.append(Token(TokenType.IDENTIFIER, kw))
+                        if command == "tokenize": print(f"{kw.upper()} {kw} null")
+                    else: 
+                        if command == "tokenize": print(f"IDENTIFIER {kw} null")
                 
                 else:
-                    print("[line %s] Error: Unexpected character: %s" % (i+1, c), file=sys.stderr)
+                    if command == "tokenize": print("[line %s] Error: Unexpected character: %s" % (i+1, c), file=sys.stderr)
                     error = True
 
-    print("EOF  null")
+    if command == "tokenize": print("EOF  null")
+    tokens.append(Token(TokenType.EOF, "null"))
+
+    if command == "parse":
+        for i in range(len(tokens)-1):
+            print(tokens[i].value, end='')
+            if tokens[i].token_type == TokenType.PAREN and tokens[i].value == '(':
+                print("group", end=' ')
+
     if error:
         exit(65)
     else:
